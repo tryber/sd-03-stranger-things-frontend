6a602f6ecf3250ee3703bcb64bc158b8195d570d
DevT <aguiarthiag@gmail.com>
2020-09-17 19:09:34 -0300

    REACT_APP

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -108,7 +108,7 @@ class StrangerThings extends React.Component {
           this.state.hereIsTheUpsideDownWorld
         )}`}
       >
-        <h1>{process.env.ENVIROMENT_TAG}</h1>
+        <h1>{process.env.REACT_APP_ENVIROMENT}</h1>
         <div className="content strangerfy">
           <div className="change-reality">
             <button onClick={this.changeRealityClick}>
