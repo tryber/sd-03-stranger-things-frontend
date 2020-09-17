a960359a05f4c9768d82eaa87f8f1896ad691c5e
Jafet Henrique <jafetguerra6@gmail.com>
2020-09-17 17:30:35 -0300

    enviroment tag

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -104,6 +104,7 @@ class StrangerThings extends React.Component {
           this.state.hereIsTheUpsideDownWorld
         )}`}
       >
+        <span>{process.env.ENVIRONMENT}</span>
         <div className="content strangerfy">
           <div className="change-reality">
             <button onClick={this.changeRealityClick}>
