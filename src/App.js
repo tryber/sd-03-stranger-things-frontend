3a0ee8b61a969d8988e8731a4b8b633c6c4648b4
DevT <aguiarthiag@gmail.com>
2020-09-17 18:28:42 -0300

    Enviroment identifier

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+      <h1>{process.env.ENVIROMENT}</h1>
       <StrangerThings />
     </div>
   );
