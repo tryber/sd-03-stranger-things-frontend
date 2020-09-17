bc81d9923a5966788ddf5c735c94008d56a2bf99
Hfreitas <hfreitas86@gmail.com>
2020-09-17 18:13:08 -0300

    configurando para uso de variaveis de ambiente

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
 
+const {
+  REACT_APP_HAWKINS_URL = 'http://localhost:3002',
+  REACT_APP_HAWKINS_TIMEOUT = 30000,
+  REACT_APP_UPSIDEDOWN_URL = 'http://localhost:3003',
+  REACT_APP_UPSIDEDOWN_TIMEOUT = 30000,
+} = process.env;
+
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: REACT_APP_HAWKINS_URL,
+  timeout: REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: REACT_APP_UPSIDEDOWN_URL,
+  timeout: REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
