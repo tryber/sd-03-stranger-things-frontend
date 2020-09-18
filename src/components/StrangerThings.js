f0ecff6a07c423f18f00d825825015f45c03e863
rdgbrezolin <brezolinr@gmail.com>
2020-09-18 14:59:04 -0300

    deploy

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
-require('dotenv').config();
+require('dotenv/config');
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
@@ -8,12 +8,12 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 
 const strangerThingsConfig = {
   url: process.env.REACT_APP_HAWKINS_URL,
-  timeout: parseInt(process.env.REACT_APP_HAWKINS_TIMEOUT, 10),
+  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
   url: process.env.REACT_APP_UPSIDEDOWN_URL,
-  timeout: parseInt(process.env.REACT_APP_UPSIDEDOWN_TIMEOUT, 10),
+  timeout: process.env.EACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
@@ -105,7 +105,6 @@ class StrangerThings extends React.Component {
           this.state.hereIsTheUpsideDownWorld
         )}`}
       >
-        <span>{process.env.REACT_APP_ENVIRONMENT_TAG}</span>
         <div className="content strangerfy">
           <div className="change-reality">
             <button onClick={this.changeRealityClick}>
