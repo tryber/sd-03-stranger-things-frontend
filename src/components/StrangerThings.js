0894f6b766c2d0b3a22f8c8b439c47bc2f6fcfd0
Henrique Rezende <henriquerezendec@gmail.com>
2020-09-19 16:57:09 -0300

    Adicionando react app na variavel

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -112,7 +112,7 @@ class StrangerThings extends React.Component {
               Mudar de Realidade
             </button>
           </div>
-          <p>{process.env.DEVELOPMENT}</p>
+          <p>{process.env.REACT_APP_DEVELOPMENT}</p>
           <div>
             <input
               placeholder="Nome do Personagem"
