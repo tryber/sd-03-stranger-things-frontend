c568da9445821a7a8bee725e769dd35cec08889e
wendersonbelko <contato@wendersonbelko.com>
2020-10-24 14:50:02 -0300

    agora vai?

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -105,6 +105,7 @@ class StrangerThings extends React.Component {
           this.state.hereIsTheUpsideDownWorld
         )}`}
       >
+        
         <div>Em desenvolvimento</div>
         <div className="content strangerfy">
           <div className="change-reality">
