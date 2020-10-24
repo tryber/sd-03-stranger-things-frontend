ccc6384e6df885431cb911ce3834d18dbeecc3e4
wendersonbelko <contato@wendersonbelko.com>
2020-10-24 14:44:49 -0300

    não é possivel que seja isso

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -105,7 +105,7 @@ class StrangerThings extends React.Component {
           this.state.hereIsTheUpsideDownWorld
         )}`}
       >
-        <div>em desenvolvimento</div>
+        <div>Em desenvolvimento</div>
         <div className="content strangerfy">
           <div className="change-reality">
             <button onClick={this.changeRealityClick}>
