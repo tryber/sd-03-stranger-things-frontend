d050fc142ea39a99390702095b3374de903362ba
Lucas Franca <luc.rangel90@gmail.com>
2020-10-30 12:06:02 -0300

    prod

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -106,8 +106,8 @@ class StrangerThings extends React.Component {
           this.state.hereIsTheUpsideDownWorld
         )}`}
       >
-        {/* <div>Produção</div> */}
-        <p>Em desenvolvimento</p>
+
+        {/* <p>Em desenvolvimento</p> */}
         <div className="content strangerfy">
           <div className="change-reality">
             <button onClick={this.changeRealityClick}>
